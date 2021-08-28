3b49eb20c15937cc2bb76ff7a3d2652cd63e4f2c
Andrey Korotkiy <andreikorotkiy@gmail.com>
2021-08-29 00:46:25 +0300

    types

--- Test.hs
+++ Test.hs
@@ -2,10 +2,15 @@ module Test where
 
 sayHello = putStrLn "Hello World from module Test!"
 
+-- Обьявление типа 
+discount :: Double -> Double -> Double -> Double
+standardDiscount :: Double -> Double
 
--- Частитчное приминение фунции
+-- Частитчное применение фунции
 let discount limit proc sum = if sum >= limit then sum * (100-proc)/100 else sum
 
 let standartDiscount = discount 1000 5
 
 -- standartDiscount 2000 > 900
+
+
