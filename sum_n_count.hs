1303338e1a42d792241c33fbb239f3c6f9aba5fc
Andrey Korotkiy <andreikorotkiy@gmail.com>
2021-09-20 00:45:27 +0300

    Решил задачу!

--- sum_n_count.hs
+++ sum_n_count.hs
@@ -6,8 +6,16 @@
 sum'n'count :: Integer -> (Integer, Integer)
 sum'n'count x | x == 0 = (0, 1)
               | otherwise = let
-                             helper ac1 ac2 1 = (ac1, ac2)
-                             helper ac1 ac2 n = helper (ac1 + abs(n) `mod` 10 + abs(n) `div` 10) (ac2 + 1) (abs (n) `div` 10)
+                             helper ac1 ac2 0 = (ac1, ac2)
+                             helper ac1 ac2 n = helper (ac1 + abs(n) `mod` 10) (ac2 + 1) (abs (n) `div` 10)
                             in helper 0 0 x
 
+-- Ура!)
 
+-- Другие решения:
+
+sum'n'count x = helper 0 0 (abs x)
+    where
+            helper 0 0 0       = (0, 1)
+            helper sum count 0 = (sum, count)
+            helper sum count value = helper (sum + mod value 10 ) (count + 1) (div value 10)
