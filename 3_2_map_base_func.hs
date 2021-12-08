74587e4fe2fc6d0f810afdf175f57f2dc5d6026c
Andrey Korotkiy <andreikorotkiy@gmail.com>
2021-12-09 01:02:54 +0300

    Функции высших порядков над map

---
+++ 3_2_map_base_func.hs
@@ -0,0 +1,46 @@
+import Prelude hiding (and, or, all, any)
+
+and, or :: [Bool] -> Bool
+
+-- Если все значения истина - Истина, если хоть одно ложное - Ложь
+and []     = True
+and (x:xs) = x && and xs  -- рекурсивно разбираем список
+
+-- Тоже самое, но или
+or []      = False
+or (x:xs)  = x || or xs
+
+-- применяет унарный предикат ко всем элементам списка
+all :: (a -> Bool) -> [a] -> Bool
+all p = and . map p
+
+-- Prelude> all odd [1, 3, 4, 54, 4]
+-- False
+
+-- присутсвует в нашем списке хотя бы один элемент, который удовлетворяет нашему предикату
+any :: (a -> Bool) -> [a] -> Bool
+any p = or . map p
+
+-- Prelude> any odd [1, 3, 4, 54, 4]
+-- True
+
+-- Prelude> any odd [2, 2]
+-- False
+
+-- Пример:
+-- Нужно взять текст и развернуть каждое слово в нем (функция revWords)
+
+-- 1) Разиваем текст на отдельные слова с помощью функции words
+-- 2) После этого нужно будет собирать список обратно - функция unwords
+--    unwords . words $ "Abc is not ABC"
+-- 3) Теперь между вызовами words и unwords нужно применять reverse к каждому элементу списка
+--    unwords . map reverse . words $ "Abc is not ABC"
+
+-- В итоге
+revWords :: String -> String
+let revWords = unwords . map reverse . words
+
+
+-- Prelude> let revWords = unwords . map reverse . words
+-- Prelude> revWords "Abc is not ABC"
+-- "cbA si ton CBA"
