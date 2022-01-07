c61253759134e2665f9f2cdf62d90af62f9f2850
Andrey Korotkiy <andreikorotkiy@gmail.com>
2022-01-08 02:48:49 +0300

    Задача evenOnly

---
+++ 3_5_evenOnly.hs
@@ -0,0 +1,19 @@
+-- Используя однократный вызов свертки, реализуйте функцию evenOnly, 
+-- которая выбрасывает из списка элементы, стоящие на нечетных местах, оставляя только четные.
+
+-- GHCi> evenOnly [1..10]
+-- [2,4,6,8,10]
+-- GHCi> evenOnly ['a'..'z']
+-- "bdfhjlnprtvxz"
+
+evenOnly :: [a] -> [a]
+-- evenOnly = foldr (\x (s, n) -> if odd $ x !! n == True then (x + s, n +1) else (s, n + 1)) (0, 0) 
+-- 1,2,3]!!1
+
+evenOnly = (foldr (\(x,s) y -> if (even x) then s:y else y) []) . foo
+foo xs = zipWith (,) [1..(length xs)] xs
+
+-- Решения из комментов
+evenOnly = snd . foldr (\a (xs, ys) -> (a : ys, xs)) ([], [])
+
+evenOnly = map snd . filter (even . fst) . zip [1 ..]
